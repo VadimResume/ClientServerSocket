9ecfc854986d3d50e17d21f8742103639c90d475
VadimMiVaRi <tulimaku@googlemail.com>
2021-08-28 06:23:17 +0300

    Update

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -8,22 +8,29 @@ public class Server {
         try (ServerSocket server = new ServerSocket(8000);) {
             System.out.println("Server started!");
 
-            try (
-                    Socket socket = server.accept();
-                    BufferedWriter writer =
-                            new BufferedWriter(
-                                    new OutputStreamWriter(
-                                            socket.getOutputStream()));
-                    BufferedReader reader =
-                            new BufferedReader(
-                                    new InputStreamReader(
-                                            socket.getInputStream()));
+            while (true)
+                try (
+                        Socket socket = server.accept();
+                        BufferedWriter writer =
+                                new BufferedWriter(
+                                        new OutputStreamWriter(
+                                                socket.getOutputStream()));
+                        BufferedReader reader =
+                                new BufferedReader(
+                                        new InputStreamReader(
+                                                socket.getInputStream()));
 
-            ){
-                writer.write("HELLO FROM SERVER");
-                writer.newLine();
-                writer.flush();
-            }
+                ){
+                    String request = reader.readLine();
+                    System.out.println("Request " + request);
+                    String response = (int) (Math.random() * 30 - 10) + "";
+                    System.out.println("Response " + response);
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
